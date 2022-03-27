07034f3178b419e25aa536eb0712a696bc167c6d
Nonso Udenwani <P8926@fidelitybank.ng>
2022-03-28 00:41:42 +0100

    MissingArrayNumber

---
+++ DataStructures/GetMissingNumber/MissingArrayNumber.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace AppZoneTest
+{
+    internal partial class Program
+    {
+        public  class MissingArrayNumber
+        {
+            public static int GetMissingNumber(int[] num)
+            {
+                if (num.Length == 0)
+                {
+                    return 0;
+                }
+
+                Array.Sort(num);
+                var value = num[0];
+                var result = num[0];
+                var count = 0;
+                while (value == result)
+                {
+                    count++;
+
+                    value = num[count];
+                    result = count + num[0];
+                }
+
+                return result;
+            }
+        }
+
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AppZoneTest
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = MissingArrayNumber.GetMissingNumber(new int [7] {9,10,16,11,14,12,15});
+            System.Console.WriteLine(result.ToString());
+        }
+
+    }
+}
